32b93d65813bd27ae8478c52d6e83ff9557e85ae
Vito <boitcov@mail.ru>
2015-09-11 23:52:30 +0300

    added typical_olymp

---
+++ code/typical_olymp.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <memory.h>
+
+#include <vector>
+#include <queue>
+
+#define file_name "f"
+
+using namespace std;
+
+int n, m, arr[100000];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	freopen(file_name".in", "r", stdin);
+	freopen(file_name".out", "w", stdout);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+}
