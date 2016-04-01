5acb2e111fa9d79985cea6824735047f35600323
JohnHook059 <johnhook059@gmail.com>
2016-04-02 04:13:45 +0600

    Массовые операции

--- code/segment_tree.cpp
+++ code/segment_tree.cpp
@@ -1,15 +1,41 @@
-#include <vector>
-
 template <typename T>
-struct tree {
-    std::vector<T> base;
+class tree {
+private:
+    vector<T> base, flag, pub;
     T (*comp)(const T &, const T &);
     T neutral;
-    tree(const std::vector<T> &v, T (*comp)(const T &, const T &), T neutral) : comp(comp), neutral(neutral) {
+    
+    void relax_flag(int v) {
+        if (v * 2 < flag.size()) flag[v * 2] = comp(flag[v * 2], flag[v]);
+        if (v * 2 + 1 < flag.size()) flag[v * 2 + 1] = comp(flag[v * 2 + 1], flag[v]);
+        flag[v] = neutral;
+    }
+    void _public_update(int left, int right, T new_value, int v, int l, int r) {
+        if (right <= l || left >= r) return;
+        if (left <= l && right >= r) {
+            flag[v] = comp(flag[v], new_value);
+            return;
+        }
+        relax_flag(v);
+        _public_update(left, right, new_value, v * 2, l, (l + r) / 2);
+        _public_update(left, right, new_value, v * 2 + 1, (l + r) / 2, r);
+        pub[v] = comp(comp(pub[v * 2], flag[v * 2] * (r - l) / 2), comp(pub[v * 2 + 1], flag[v * 2 + 1] * (r - l) / 2)); // TODO * -> T public_comp()
+    }
+    T _public_get(int left, int right, int v, int l, int r) {
+        if (right <= l || left >= r) return neutral;
+        if (left <= l && right >= r)
+            return comp(pub[v], flag[v] * (r - l)); // TODO * -> T public_comp()
+        relax_flag(v);
+        return comp(_public_get(left, right, v * 2, l, (l + r) / 2), _public_get(left, right, v * 2 + 1, (l + r) / 2, r));
+    }
+public:
+    tree(const vector<T> &v, T (*comp)(const T &, const T &), T neutral) : comp(comp), neutral(neutral) {
         int st = 1;
         while (st < v.size())
             st <<= 1;
         base.resize(st << 1);
+        flag.resize(st << 1);
+        pub.resize(st << 1);
         for (int i = st; i < st + v.size(); ++i)
             base[i] = v[i - st];
         for (int i = st + int(v.size()); i < st << 1; ++i)
@@ -17,7 +43,7 @@ struct tree {
         for (int i = st - 1; i > 0; --i)
             base[i] = comp(base[i << 1], base[(i << 1) + 1]);
     }
-    void update(int pos, T new_value) {
+    void single_update(int pos, T new_value) {
         pos += base.size() >> 1;
         base[pos] = new_value;
         while (pos > 1) {
@@ -25,26 +51,36 @@ struct tree {
             base[pos] = comp(base[pos << 1], base[(pos << 1) + 1]);
         }
     }
-    T get(int l, int r) {
+    void public_update(int left, int right, T new_value) {
+        _public_update(left, right, new_value, 1, 0, int(base.size()) / 2);
+    }
+    T single_get(int left, int right) {
         T ans = neutral;
-        l += base.size() >> 1;
-        r += base.size() >> 1;
-        while (l < r) {
-            if (l % 2) ans = comp(ans, base[l++]);
-            l /= 2;
-            if (r % 2) ans = comp(ans, base[--r]);
-            r /= 2;
+        left += base.size() >> 1;
+        right += base.size() >> 1;
+        while (left < right) {
+            if (left % 2) ans = comp(ans, base[left++]);
+            left >>= 1;
+            if (right % 2) ans = comp(ans, base[--right]);
+            right >>= 1;
         }
         return ans;
     }
+    T public_get(int left, int right) {
+        return _public_get(left, right, 1, 0, int(base.size()) / 2);
+    }
+    T get(int left, int right) {
+        return comp(single_get(left, right), public_get(left, right));
+    }
 };
 
-// Authors = JohnHook
+
 // Дерево полуинтервалов.
 // В конструктор подается:
 //     1. Вектор элементов того же типа, что и дерево.
 //     2. Функция по которой строится дерево.
 //     3. Нейтральный элемент.
 // Например: tree<int> t(array, sum, 0);
-// Изменение элемента: update(позиция изменяемого элемента, новое значение)
+// Изменение элемента: single_update(позиция изменяемого элемента, новое значение)
+// Массовая операция изменения элементов: public_update(left, right, value)
 // Запрос: get(левая граница, правая граница(не включительно))
